4e6a79f6b31e5d7342d187a57db60dbae4ab1100
gorkemcamusoglu00 <gorkem.camusoglu75@gmail.com>
2026-04-28 15:42:00 +0300

    taskmanager app was created

---
+++ src/main/java/org/example/taskmanager1/Taskmanager1Application.java
@@ -0,0 +1,13 @@
+package org.example.taskmanager1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taskmanager1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taskmanager1Application.class, args);
+	}
+
+}
